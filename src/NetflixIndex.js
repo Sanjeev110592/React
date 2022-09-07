1aa7425c380ea0764f0f803808faefeea776d679
Sanjeev Kumar <sanjeev110592@gmail.com>
2022-09-07 21:48:38 +0530

    backgroundBanner

---
+++ src/NetflixIndex.js
@@ -0,0 +1,12 @@
+import "./NetflixIndex.css" ;
+export function NetflixIndex(){
+    return(
+        <>
+          <div className="banner">
+            <div className="banner_alpha">
+
+            </div>
+          </div>
+        </>
+    )
+}
