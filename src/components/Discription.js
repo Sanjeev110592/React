52ffc89e70b8ded43c906e3204a5bc9f5d63fb5d
Sanjeev Kumar <sanjeev110592@gmail.com>
2022-09-12 13:37:52 +0530

    pics

--- src/components/Discription.js
+++ src/components/Discription.js
@@ -10,9 +10,24 @@ export function Discription(){
         {Name: "Nike Casuals", Price:7000.42}
      ];
      const product = [
-        {Name: "Sony Speaker", Price:5600.32, Photo:"", Rating:{Rate:4.2, Count:3000}},
-        {Name: "Nike Casuals", Price:7000.42, Photo:"https://images.pexels.com/photos/2529148/pexels-photo-2529148.jpeg?auto=compress&cs=tinysrgb&w=1600", Rating:{Rate:3.6, Count:6020}},
-        {Name: "Winter Jacket", Price:2400.44, Photo:"https://images.pexels.com/photos/157675/fashion-men-s-individuality-black-and-white-157675.jpeg?auto=compress&cs=tinysrgb&w=1600", Rating:{Rate:4.6, Count: 446}}
+        {Name: "Sony Speaker", Price:5600.32, Photo:"images/speaker.jpeg", Rating:{Rate:4.2, Count:3000}},
+        {Name: "Nike Casuals", Price:7000.42, Photo:"images/shoe.jpg", Rating:{Rate:3.6, Count:6020}},
+        {Name: "Winter Jacket", Price:2400.44, Photo:"images/jacket.jpeg", Rating:{Rate:4.6, Count: 446}},
+        {Name: "Sony Speaker", Price:5600.32, Photo:"images/speaker.jpeg", Rating:{Rate:4.2, Count:3000}},
+        {Name: "Nike Casuals", Price:7000.42, Photo:"images/shoe.jpg", Rating:{Rate:3.6, Count:6020}},
+        {Name: "Winter Jacket", Price:2400.44, Photo:"images/jacket.jpeg", Rating:{Rate:4.6, Count: 446}},
+        {Name: "Sony Speaker", Price:5600.32, Photo:"images/speaker.jpeg", Rating:{Rate:4.2, Count:3000}},
+        {Name: "Nike Casuals", Price:7000.42, Photo:"images/shoe.jpg", Rating:{Rate:3.6, Count:6020}},
+        {Name: "Winter Jacket", Price:2400.44, Photo:"images/jacket.jpeg", Rating:{Rate:4.6, Count: 446}},
+        {Name: "Sony Speaker", Price:5600.32, Photo:"images/speaker.jpeg", Rating:{Rate:4.2, Count:3000}},
+        {Name: "Nike Casuals", Price:7000.42, Photo:"images/shoe.jpg", Rating:{Rate:3.6, Count:6020}},
+        {Name: "Winter Jacket", Price:2400.44, Photo:"images/jacket.jpeg", Rating:{Rate:4.6, Count: 446}},
+        {Name: "Sony Speaker", Price:5600.32, Photo:"images/speaker.jpeg", Rating:{Rate:4.2, Count:3000}},
+        {Name: "Nike Casuals", Price:7000.42, Photo:"images/shoe.jpg", Rating:{Rate:3.6, Count:6020}},
+        {Name: "Winter Jacket", Price:2400.44, Photo:"images/jacket.jpeg", Rating:{Rate:4.6, Count: 446}},
+        {Name: "Sony Speaker", Price:5600.32, Photo:"images/speaker.jpeg", Rating:{Rate:4.2, Count:3000}},
+        {Name: "Nike Casuals", Price:7000.42, Photo:"images/shoe.jpg", Rating:{Rate:3.6, Count:6020}},
+        {Name: "Winter Jacket", Price:2400.44, Photo:"images/jacket.jpeg", Rating:{Rate:4.6, Count: 446}},
      ];
     return(
         <div className="category">
@@ -58,18 +73,19 @@ export function Discription(){
                 </tbody>
             </table>
             <h2>Card</h2>
-            <div>
+            <div className="parent">
                 {
                     product.map(product=>
-                    <div>
-                        <img src={product.Photo} width="150px" height="200px" alt="look"/>
+                    <div className="card" key={product}>
+                        <img src={product.Photo} width="100%" height="200px" alt="look"/>
                         <h3>{product.Name}</h3>
                         <dl>
-                            <dt>Price</dt>
-                            <dd>{product.Price}</dd>
-                            <dt>Rating</dt>
+                            <dt>Price :</dt>
+                            <dd key={product.Price}>{product.Price}</dd>
+                            <dt>Rating :</dt>
                             <dd>{product.Rating.Rate}[{product.Rating.Count}]</dd>
                         </dl>
+                        <button>Add to cart</button>
                     </div>
 
                     )
